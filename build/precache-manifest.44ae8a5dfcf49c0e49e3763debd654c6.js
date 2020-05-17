55b4c148f8947ac5711e159914477bcf15fcaad5
Shyan <shyan02@gmail.com>
2020-05-18 00:05:04 +0800

    After npm run build

---
+++ build/precache-manifest.44ae8a5dfcf49c0e49e3763debd654c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "02bcf379979000efe5d8",
+    "url": "/static/js/main.02bcf379.chunk.js"
+  },
+  {
+    "revision": "57c420c5e2fcf954e048",
+    "url": "/static/js/1.57c420c5.chunk.js"
+  },
+  {
+    "revision": "02bcf379979000efe5d8",
+    "url": "/static/css/main.9b7f8682.chunk.css"
+  },
+  {
+    "revision": "57c420c5e2fcf954e048",
+    "url": "/static/css/1.e665fe0d.chunk.css"
+  },
+  {
+    "revision": "cc056ce148061ef8fbdba3603888144a",
+    "url": "/index.html"
+  }
+];
