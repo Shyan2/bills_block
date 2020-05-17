e74c5e8093e88d7c14aed7bef2bee6da67d54237
Shyan <shyan02@gmail.com>
2020-05-18 01:14:05 +0800

    New build

---
+++ build/precache-manifest.fd1895fc1fc032fd02c99c1a0daa84c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53de1b00aadf56bd3b0e",
+    "url": "/blockgame/static/js/runtime~main.53de1b00.js"
+  },
+  {
+    "revision": "72bbd14754a337937a1d",
+    "url": "/blockgame/static/js/main.72bbd147.chunk.js"
+  },
+  {
+    "revision": "e5c19bbebf9f37b60ce8",
+    "url": "/blockgame/static/js/1.e5c19bbe.chunk.js"
+  },
+  {
+    "revision": "72bbd14754a337937a1d",
+    "url": "/blockgame/static/css/main.9b7f8682.chunk.css"
+  },
+  {
+    "revision": "e5c19bbebf9f37b60ce8",
+    "url": "/blockgame/static/css/1.e665fe0d.chunk.css"
+  },
+  {
+    "revision": "490e8aeee231329f495f10cadbcb3d71",
+    "url": "/blockgame/index.html"
+  }
+];
